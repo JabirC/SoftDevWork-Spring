d443604bbda9b746f8086a189e2e0e105c0451d6
JabirC <jchowdhury@stuy.edu>
2019-04-16 12:37:21 -0400

    update

--- 18_listcomp/listcomp.py
+++ 18_listcomp/listcomp.py
@@ -1,3 +1,8 @@
+ # Jabir Chowdhury
+ # Softdev2 pd6
+ # K18 -- Listcomp
+ # 2019-04-15
+
 def pyth(n):
      return [(a,b,int((a*a + b*b)**.5)) for a in range(1,n) for b in range(a+1,n) if int((a*a + b*b)**.5) < n and ((a*a + b*b)**.5).is_integer()]
 print(pyth(10000))
